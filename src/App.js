b7407e027c6310e24ff801fbb924bf00ddee655c
unknown <aroramahak1301@gmail.com>
2024-05-29 21:38:05 +0530

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
+// src/App.js
+import React, { useState } from 'react';
 import './App.css';
+import Header from './components/Header';
+import SearchBar from './components/SearchBar';
+import CategorySort from './components/CategorySort';
+import ImageGallery from './components/ImageGallery';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [selectedCategory, setSelectedCategory] = useState('all');
+
+    return (
+        <div className="App">
+            <Header />
+            <SearchBar />
+            <CategorySort onCategoryChange={setSelectedCategory} />
+            <ImageGallery selectedCategory={selectedCategory} />
+        </div>
+    );
 }
 
 export default App;
